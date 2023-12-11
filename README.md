e11192ec9c7addef1270a85135036bfebe94706b
mehulhere <themehulpahuja@gmail.com>
2023-12-11 17:25:21 +0530

    feat: No need to move, downloadsCleaner file anywhere, Auto Run script with bat file, added a dictionary for proper tracking of extensions. Bug: Removed ambiguity in naming: 'Images & 'Pictures', Design: Improved Some Design Aspects

---
+++ README.md
@@ -0,0 +1,26 @@
+# cleanurdownloads
+A simple Python script to organise your downloads folder
+
+## Requirements
+- You need to have python installed in your system
+- This code was tested and created on a Windows system, so there's no guarantee if it will work on a Mac or a Linux system. It should ideally work, but no promises
+- Download the provided python file in your donwloads directory (make sure the dowanloadsCleaner.py file itself is in downloads, and not in any subfolder)
+
+## How to use (Method 1) (Easier)
+- Simple click the python file to run it
+
+## How to use (Method 2)
+- Open up terminal, navigate to Downloads using cd
+- run the given command
+```
+py downloadsCleaner.py
+```
+- After the terminal prints the success message, open up your file explorer and check for results
+
+
+## How to extend the script to include custom file extensions 
+It's a fairly simple code, and actually works on any folder to be used to organise. Just run in in the folder you want to organise.
+
+Anyways back to customising the code, you simply need to add in the extension names required on the required lists. And if you want to add any new kind of folder, simply add it in the two lines where the folder names are declared. 
+
+I will probably add a few comments to indicate where to edit the code incase it's not too clear
---
+++ downloadsCleaner.py
@@ -0,0 +1,94 @@
+import os
+import time
+
+original_path = os.getcwd()
+downloads_path = os.path.join(os.path.expanduser('~'), 'Downloads')
+os.chdir(downloads_path)
+path = os.getcwd()
+
+file_extensions = {
+    "images": ["webp", "png", "jpg", "svg", "jpeg", "gif", "cr2", "bmp", "ico"],
+    "music": ["mp3", "wav"],
+    "videos": ["mp4", "mov", "mkv"],
+    "documents": ["docx", "pdf", "txt", "html", "ppt", "odt", "xls", "csv", "rtf", "ppt", ".md"],
+    "zips": ["zip", "7zip", "rar", "tar", "gz", "bz2"],
+    "executables": ["exe", "apk", "bin", "part", "iso", "msi", "torrent", "sh"], #Can name them Drivers
+    "codes": ["c", "cpp", "py", "m", "brd", "sch", "fxml", "java", "conf", "html", "json", "xlsx", "mobi", "css", "scss", "js","ipynb"]
+}
+
+#Print Logs
+print("Initializing File Cleanup...")
+time.sleep(1)  # Simulating some work being done
+print("Hacking Your PC.....")
+time.sleep(1)
+print("JKJK!")
+time.sleep(0.5)
+print("Checking for unnecessary files...")
+time.sleep(1)  # Simulating more work
+print("Organizing remaining files...")
+
+errorFileCount = 0
+miscalFileCount = 0
+
+list_of_dirs = file_extensions.keys()
+
+#Making Directories for Sorting
+for i in list_of_dirs:
+    try:
+        os.mkdir(i)
+    except FileExistsError:
+        continue
+
+list_of_files = os.listdir()
+print("Running Cleanup in " +path)
+
+#Script For Organizing
+for i in list_of_files:
+    print("Organizing file: " +i)
+    try:
+        if i == "cleanurdownloads":
+            continue
+        elif i.split(".")[-1] in file_extensions["images"]:
+            fromname = path + "/" + i
+            toname = path + "/images/" + i  
+            os.rename(fromname, toname)
+        elif i.split(".")[-1] in file_extensions["music"]:
+            fromname = path + "/" + i
+            toname = path + "/music/" + i
+            os.rename(fromname, toname)
+        elif i.split(".")[-1] in file_extensions["videos"]:
+            fromname = path + "/" + i
+            toname = path + "/videos/" + i
+            os.rename(fromname, toname)
+        elif i.split(".")[-1] in file_extensions["documents"]:
+            fromname = path + "/" + i
+            toname = path + "/documents/" + i
+            os.rename(fromname, toname)
+        elif i.split(".")[-1] in file_extensions["zips"]:
+            fromname = path + "/" + i
+            toname = path + "/zips/" + i
+            os.rename(fromname, toname)
+        elif i.split(".")[-1] in file_extensions["executables"]:
+            fromname = path + "/" + i
+            toname = path + "/executables/" + i
+            os.rename(fromname, toname)
+        elif i.split(".")[-1] in file_extensions["codes"]:
+            fromname = path + "/" + i
+            toname = path + "/codes/" + i
+            os.rename(fromname, toname)
+        elif not os.path.isfile(os.path.join(path + "/",i)) and i not in file_extensions.keys():
+            print(i)
+            fromname = path + "/" + i
+            toname = path + "/folders/" + i
+            os.rename(fromname, toname)
+        else: #Miscallenous
+            miscalFileCount += 1
+    except (PermissionError, FileExistsError, FileNotFoundError) as e:
+        # print(f"Error: {e}")
+        errorFileCount += 1 #For counting files that give error
+        continue
+    
+fileOrganized = str(len(list_of_files)-errorFileCount -miscalFileCount)
+print(fileOrganized +" Files have been organized")
+time.sleep(0.5)
+print("Downloads cleanup completed! Enjoy the tidiness.")
